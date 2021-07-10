0aa3edd3774b5cb4b5e231e049efbec8d77f0954
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-07-10 14:15:23 -0400

    1

---
+++ week5/giphy-search/src/App.js
@@ -0,0 +1,12 @@
+import GiphyLookup from './GiphyLookup'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <GiphyLookup initialQuery='cat'/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ week5/giphy-search/src/GiphyLookup.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+function GiphyLookup({initialQuery}) {
+    const[loading, setLoading] = useState(false);
+    const[error, setError] = useState(false);
+    const[imgSrc, setImageSrc] = useState('');
+    const[alt, setAlt] = useState('');
+
+    const fetchImage = (query = initialQuery ) =>{
+
+        setLoading(true);
+        const encodedQuery = encodeURIComponent(query);
+        const url = `https://api.giphy.com/v1/gifs/random?api_key-StGEROY7n9630aOR3Cn3JMMHPbNnEXwo&tag=${encodedQuery}`;
+
+        fetch(url, {
+            method:'GET',
+        }).then(function(response) {
+            if (response.ok) {
+                return response.json();
+            }
+                return Promise.reject(response);
+        }).then(function({data}){
+            setLoading(false);
+            setImageSrc(data.fixed_height_downsampled_url);
+            setAlt(data.title)
+        }).catch(function(error){
+            setLoading(false);
+            setError(true);
+            console.warn('something went wrong',error);
+        });
+    }
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        fetchImage(evt.target.query.value)
+    }
+    useEffect(fetchImage, [initialQuery]);
+
+    return (
+        <div>
+            <br></br>
+            <header>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="query" placeHolder="search phrase" style={{width:"225px"}}/> &nbsp;
+                    <button type="submit">Enter Search Words Here</button>
+                </form>
+            </header>
+            <br></br>
+            <main>
+                <section>
+                    {loading &&<div>Loading...</div>}
+                    {error &&<div>Error</div>}
+                    {!loading && !error && !imgSrc &&
+                    <div>Nothing Found</div>}
+                     {!loading && !error && !imgSrc &&
+                     <div><img src={imgSrc} alt = {alt} style={{width:'300px'}}/></div>
+                     }
+
+                </section>
+            </main>
+        </div>
+    )
+                    }
+
+   //
+    export default GiphyLookup
